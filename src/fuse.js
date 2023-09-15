a3d308263a84ee4a72feb81d2329e052acfa6d5e
Guilherme <delarryguilherme@gmail.com>
2023-09-15 12:36:56 -0300

    Adição de arquivos
    Adição de arquivos

---
+++ src/fuse.js
@@ -0,0 +1,38 @@
+import Fuse from "fuse.js"; //biblioteca do metodo fuzzy
+import { musicas } from './musicas.js'
+
+//opções de configuração da biblioteca
+const fuse_config = {
+  includeScore: true,
+  shouldSort: true,
+  location: 0,
+  minMatchCharLength: 3,
+  threshold: 0.6,
+  distance: 100,
+  fieldNormWeight: 0,
+  keys: ["nome"],
+};
+
+const fuse = new Fuse(musicas, fuse_config);
+let palavras_chave
+
+const botao = document.querySelector("#botao")
+const lista = document.createElement('ul')
+
+botao.addEventListener("click", exibirFuse);
+
+export function exibirFuse() {
+  palavras_chave = document.querySelector("input").value //atribui o valor do input a variavel
+  lista.innerHTML = null //limpa a lista a cada nova consulta
+  const resultado = fuse.search(palavras_chave); //metodo de busca em acao
+
+  //loop que insere cada item em uma lista do html
+  for (let i = 0; i < (resultado.length); i++) {
+    let elemento_lista = document.createElement("li");
+    elemento_lista.innerHTML = `<p>${resultado[i].item.nome} - <span id="score">${resultado[i].score}</span></p>`;
+    lista.appendChild(elemento_lista);
+  }
+
+  //exibe
+  document.querySelector("#resultados").appendChild(lista)
+}
